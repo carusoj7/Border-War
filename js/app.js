6f1aa6b8143a38f20f1e12057c96fda0186b6fb5
Joe Caruso <carusoj7@gmail.com>
2023-04-01 10:45:55 -0500

    create two divs to place flip cards

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 // Declare deck variables
 const starterDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
-let deck1, deck2
+let playerDeck, computerDeck
 let cardToRemove
-let shuffledDeck = []
 // Cached element references
 let playerDeckEl = document.getElementById("playerDeck")
 let computerDeckEl = document.getElementById("computerDeck")
@@ -25,15 +24,15 @@ function shuffle() {
 }
 
 function deal() { 
-  deck1 = []
-  deck2 = []
+  playerDeck = []
+  computerDeck = []
   starterDeck.forEach((card, i) => {
     if (i % 2 === 1) {
-      deck1.push(card)
+      playerDeck.push(card)
     } else if (starterDeck[i] % 2 !== 1) {
-      deck2.push(card)
+      computerDeck.push(card)
     }
   }
   )}
-console.log(deck1);
-console.log(deck2);
+console.log(playerDeck);
+console.log(computerDeck);
