b82be3bcafdb9daa09e28993e23972e947f80f4b
Joe Caruso <carusoj7@gmail.com>
2023-04-06 19:37:14 -0500

    update css

--- js/app.js
+++ js/app.js
@@ -1,62 +1,62 @@
 const cards = [
-  {card: "dA", value: 14},
-  {card: "dQ", value: 12},
-  {card: "dK", value:13},
-  {card: "dJ", value: 11},
-  {card: "d10", value: 10},
-  {card: "d09", value: 9},
-  {card: "d08", value: 8},
-  {card: "d07", value: 7},
-  {card: "d06", value: 6},
-  {card: "d05", value: 5},
-  {card: "d04", value: 4},
-  {card: "d03", value: 3},
-  {card: "d02", value: 2},
-  {card: "hA", value: 14},
-  {card: "hQ", value: 12},
-  {card: "hK", value: 13},
-  {card: "hJ", value: 11},
-  {card: "h10", value: 10},
-  {card: "h09", value: 9},
-  {card: "h08", value: 8},
-  {card: "h07", value: 7},
-  {card: "h06", value: 6},
-  {card: "h05", value: 5},
-  {card: "h04", value: 4},
-  {card: "h03", value: 3},
-  {card: "h02", value: 2},
-  {card: "cA", value: 14},
-  {card: "cQ", value: 12},
-  {card: "cK", value: 13},
-  {card: "cJ", value: 11},
-  {card: "c10", value: 10},
-  {card: "c09", value: 9},
-  {card: "c08", value: 8},
-  {card: "c07", value: 7},
-  {card: "c06", value: 6},
-  {card: "c05", value: 5},
-  {card: "c04", value: 4},
-  {card: "c03", value: 3},
-  {card: "c02", value: 2},
-  {card: "sA", value: 14},
-  {card: "sQ", value: 12},
-  {card: "sK", value: 13},
-  {card: "sJ", value: 11},
-  {card: "s10", value: 10},
-  {card: "s09", value: 9},
-  {card: "s08", value: 8},
-  {card: "s07", value: 7},
-  {card: "s06", value: 6},
-  {card: "s05", value: 5},
-  {card: "s04", value: 4},
-  {card: "s03", value: 3},
-  {card: "s02", value: 2},
+  { card: "dA", value: 14 },
+  { card: "dQ", value: 12 },
+  { card: "dK", value: 13 },
+  { card: "dJ", value: 11 },
+  { card: "d10", value: 10 },
+  { card: "d09", value: 9 },
+  { card: "d08", value: 8 },
+  { card: "d07", value: 7 },
+  { card: "d06", value: 6 },
+  { card: "d05", value: 5 },
+  { card: "d04", value: 4 },
+  { card: "d03", value: 3 },
+  { card: "d02", value: 2 },
+  { card: "hA", value: 14 },
+  { card: "hQ", value: 12 },
+  { card: "hK", value: 13 },
+  { card: "hJ", value: 11 },
+  { card: "h10", value: 10 },
+  { card: "h09", value: 9 },
+  { card: "h08", value: 8 },
+  { card: "h07", value: 7 },
+  { card: "h06", value: 6 },
+  { card: "h05", value: 5 },
+  { card: "h04", value: 4 },
+  { card: "h03", value: 3 },
+  { card: "h02", value: 2 },
+  { card: "cA", value: 14 },
+  { card: "cQ", value: 12 },
+  { card: "cK", value: 13 },
+  { card: "cJ", value: 11 },
+  { card: "c10", value: 10 },
+  { card: "c09", value: 9 },
+  { card: "c08", value: 8 },
+  { card: "c07", value: 7 },
+  { card: "c06", value: 6 },
+  { card: "c05", value: 5 },
+  { card: "c04", value: 4 },
+  { card: "c03", value: 3 },
+  { card: "c02", value: 2 },
+  { card: "sA", value: 14 },
+  { card: "sQ", value: 12 },
+  { card: "sK", value: 13 },
+  { card: "sJ", value: 11 },
+  { card: "s10", value: 10 },
+  { card: "s09", value: 9 },
+  { card: "s08", value: 8 },
+  { card: "s07", value: 7 },
+  { card: "s06", value: 6 },
+  { card: "s05", value: 5 },
+  { card: "s04", value: 4 },
+  { card: "s03", value: 3 },
+  { card: "s02", value: 2 },
 ]
 // Declare deck variables
 let playerDeck = []
 let computerDeck = []
 let warWinner = []
-let playerCardToRemove  
+let playerCardToRemove
 let computerCardToRemove
 // Cached element references
 let playerDeckEl = document.getElementById("playerDeck")
@@ -79,9 +79,14 @@ function init() {
   playerCardEl.classList.remove(playerCardEl.card)
   playerCardEl.classList.remove(playerCardToRemove)
   playerCardEl.classList.add("outline")
+  computerDeckEl.classList.remove("outline")
+  computerDeckEl.classList.add("back-mizzou-tiger-logo","shadow")
+  playerDeckEl.classList.remove("outline")
+  playerDeckEl.classList.add("back-jayhawk","shadow")
   computerCardEl.classList.remove(computerCardEl.card)
   computerCardEl.classList.remove(computerCardToRemove)
   computerCardEl.classList.add("outline")
+  warBtn.hidden = true
   messageEl.textContent = "Click Flip Card to Begin"
   shuffle()
   renderScore()
@@ -94,10 +99,10 @@ function shuffle() {
     cards[i] = cards[j];
     cards[j] = temp;
     deal()
-  } 
+  }
 }
 
-function deal() { 
+function deal() {
   playerDeck = []
   computerDeck = []
   cards.forEach((card, i) => {
@@ -110,34 +115,38 @@ function deal() {
 }
 
 function handleClick() {
-    render()
-    compare()
-    renderScore()
+  render()
+  compare()
 }
 
 function compare() {
   console.log(playerDeck)
   console.log(computerDeck)
-  determineWinner()
   const playerCardPicked = playerDeck[0]
   const computerCardPicked = computerDeck[0]
   console.log(playerCardPicked)
   console.log(computerCardPicked)
-  if(playerCardPicked && computerCardPicked){
+  btn.hidden = false
+  warBtn.hidden = true
+  if (playerCardPicked && computerCardPicked) {
     render(playerCardPicked, computerCardPicked)
+
     if (playerCardPicked.value > computerCardPicked.value) {
-    playerDeck.push(computerDeck.shift(), playerDeck.shift(), ...warWinner)
-    warWinner = []
-    messageEl.textContent = "KU Wins Hand!"
+      playerDeck.push(computerDeck.shift(), playerDeck.shift(), ...warWinner)
+      warWinner = []
+      messageEl.textContent = "KU Wins Hand!"
     } else if (playerCardPicked.value < computerCardPicked.value) {
-    computerDeck.push(playerDeck.shift(), computerDeck.shift(), ...warWinner)
-    warWinner = []
-    messageEl.textContent = "Mizzou Wins Hand!"
-    } else if (playerCardPicked.value === computerCardPicked.value)  {
-      btn.disable = true
+      computerDeck.push(playerDeck.shift(), computerDeck.shift(), ...warWinner)
+      warWinner = []
+      messageEl.textContent = "Mizzou Wins Hand!"
+    } else if (playerCardPicked.value === computerCardPicked.value) {
+      btn.hidden = true
       messageEl.textContent = "We Have A War! Click War!"
+      warBtn.hidden = false
     }
-  } 
+  }
+  renderScore()
+  determineWinner()
 }
 
 function handleClickWar() {
@@ -147,9 +156,9 @@ function handleClickWar() {
     warBtn.disable = true
   }
   if (playerCardPicked.value === computerCardPicked.value) {
-  warWinner.push(...computerDeck.splice(0, 4), ...playerDeck.splice(0, 4))
-  console.log("war winner", warWinner);
-  compare()
+    warWinner.push(...computerDeck.splice(0, 4), ...playerDeck.splice(0, 4))
+    console.log("war winner", warWinner);
+    compare()
   }
 }
 
@@ -158,26 +167,22 @@ function determineWinner() {
   console.log(computerDeck);
   if ((playerDeck.length > 0) && (computerDeck.length < 1)) {
     messageEl.textContent = "Game Over! Jayhawks Win!"
-    playerCardEl.classList.remove(playerCardToRemove)
-    computerCardEl.classList.remove(computerCardToRemove)
-    playerCardEl.classList.add("outline")
-    computerCardEl.classList.add("outline")
     playerScoreEl.textContent = 52
-  computerScoreEl.textContent = 0
+    computerScoreEl.textContent = 0
+    computerDeckEl.classList.add("outline")
+    computerDeckEl.classList.remove("back-mizzou-tiger-logo","shadow")
   }
   if ((playerDeck.length < 1) && (computerDeck.length > 0)) {
     messageEl.textContent = "Game Over! Tigers Win!"
-    playerCardEl.classList.remove(playerCardToRemove)
-    computerCardEl.classList.remove(computerCardToRemove)
-    playerCardEl.classList.add("outline")
-    computerCardEl.classList.add("outline")
     playerScoreEl.textContent = 0
     computerScoreEl.textContent = 52
+    playerDeckEl.classList.add("outline")
+    playerDeckEl.classList.remove("back-jayhawk","shadow")
   }
 }
 
 function render(playerCard, computerCard) {
-  if (playerCard) {  
+  if (playerCard) {
     playerCardEl.classList.remove("outline")
     playerCardEl.classList.remove(playerCardToRemove)
     playerCardEl.classList.add(playerCard.card)
@@ -204,5 +209,7 @@ function endGame() {
   computerCardEl.classList.remove(computerCardToRemove)
   playerCardEl.classList.add("outline")
   computerCardEl.classList.add("outline")
+  computerDeckEl.classList.remove("back-mizzou-tiger-logo","shadow")
+  computerDeckEl.classList.add("outline")
   messageEl.textContent = "Game Over! Jayhawks Win!"
 }
