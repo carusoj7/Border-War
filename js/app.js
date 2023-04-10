336996d1e29cb53d943c02d1d6d5f2e460006379
Joe Caruso <carusoj7@gmail.com>
2023-04-10 08:13:56 -0500

    remove semicolons

--- js/app.js
+++ js/app.js
@@ -96,10 +96,10 @@ function init() {
 
 function shuffle() {
   for (let i = cards.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i);
-    let temp = cards[i];
-    cards[i] = cards[j];
-    cards[j] = temp;
+    let j = Math.floor(Math.random() * i)
+    let temp = cards[i]
+    cards[i] = cards[j]
+    cards[j] = temp
     deal()
   }
 }
